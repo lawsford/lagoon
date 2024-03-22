e490f4f3876b46850d735068b721197b9ba4f0f0
Sarim Asif <muhammadsarim.asif@elektrobit.com>
2024-03-22 16:54:30 +0100

    add configs for kind cluster

---
+++ scripts/install_terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://releases.hashicorp.com/terraform/1.7.5/terraform_1.7.5_linux_amd64.zip
+unzip terraform_1.7.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+rm terraform_1.7.5_linux_amd64.zip
+terraform -v
